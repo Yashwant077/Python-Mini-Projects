fb2cef17f3cee313ac1948ba521829b6132879d5
Yashwant077 <84570757+Yashwant077@users.noreply.github.com>
2022-04-22 10:43:20 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+# This project is of very basic level.
+# Here it simply uses basic string concatenation concept of python
+# We can concatenate strings in python in following ways:
+# for e.g. we just want to print a hello message to a person.
+
+person_name = "Yashwant"
+print("Hello "+person_name)
+print("How are you {}?".format(person_name))
+print("%s is a good boy."%person_name)
+print(f"I wish you all the best {person_name}.")
+
+print("\n")
+
+# print ('Hello World')
+# n = input('Name: ')
+# a = input('Adjective: ')
+
+n = 'Yashwant'
+a = 'coder'
+
+print(f'{n} is trying to be a good {a}')
+print('Hi '+n+'\nHow are you '+n+'?')
